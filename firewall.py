8f7b17afad1c96ec634c5af37c71fd2ed7cef3a8
Your Name <you@example.com>
2025-11-15 16:50:37 -0700

    s

--- firewall.py
+++ firewall.py
@@ -10,7 +10,7 @@ import argparse
 # Configuration variables
 LOG_FILE = "/tmp/access.log"
 OCCURRENCE_THRESHOLD = 2
-IP_OCCURRENCE_THRESHOLD = 25
+IP_OCCURRENCE_THRESHOLD = 30
 NTFY_URL = "https://push.poster.place/firewall"
 TIME_FRAME = 30  # 30 Seconds
 
