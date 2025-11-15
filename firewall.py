d15e47f239b4323b03bb8d7df051eba56c9e4155
Your Name <you@example.com>
2025-11-15 15:07:51 -0700

    s

--- firewall.py
+++ firewall.py
@@ -304,6 +304,7 @@ def main():
                     one_minute_ago.lower() in line.lower()
                     and not any(term.lower() in line.lower() for term in SKIPPED_TERMS)
                     and not any(term.lower() in line.lower() for term in FEDIVERSE_TRAFFIC)
+                    and any(term.lower() in line.lower() for term in FEDIVERSE_TRAFFIC)
                 ):
                     if ip_match:
                         ip_address = ip_match.group()
