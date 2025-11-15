e42a7f42d0c59c9dae0acfc22a89e4f5fbb8af95
Your Name <you@example.com>
2025-11-15 15:20:37 -0700

    a

--- firewall.py
+++ firewall.py
@@ -302,9 +302,9 @@ def main():
                 # Excludes Fediverse Traffic and SKIPPED_TERMS
                 if (
                     one_minute_ago.lower() in line.lower()
+                    and any(blocked.lower() in line.lower() for blocked in BLOCK_ARRAY)
                     and not any(term.lower() in line.lower() for term in SKIPPED_TERMS)
                     and not any(item.lower() in line.lower() for item in FEDIVERSE_TRAFFIC)
-                    and any(blocked.lower() in line.lower() for blocked in BLOCK_ARRAY)
                 ):
                     if ip_match:
                         ip_address = ip_match.group()
