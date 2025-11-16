c1e162381d795a72c2fcf917cf0c2e91915de4c3
Your Name <you@example.com>
2025-11-15 19:03:37 -0700

    s

--- firewall.py
+++ firewall.py
@@ -12,7 +12,7 @@ LOG_FILE = "/tmp/access.log"
 OCCURRENCE_THRESHOLD = 2
 IP_OCCURRENCE_THRESHOLD = 30
 NTFY_URL = "https://push.poster.place/logs"
-TIME_FRAME = 5 # 30 Seconds
+TIME_FRAME = 3 # 30 Seconds
 
 FEDIVERSE_TRAFFIC = {
     "husky",
