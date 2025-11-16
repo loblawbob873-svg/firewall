191e31266b28db1387f680c9d6335be591a4854e
Your Name <you@example.com>
2025-11-15 17:36:44 -0700

    sds

--- firewall.py
+++ firewall.py
@@ -252,7 +252,7 @@ SKIPPED_TERMS = [
 ]
 
 # SKIP NTFY Alerts if a word is on this list
-SKIP_ALERTS = ["already in the ruleset", "Searching"]
+SKIP_ALERTS = ["already", "searching"]
 
 # Set up logging
 logging.basicConfig(
