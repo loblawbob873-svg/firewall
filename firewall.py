beed52db946206fe33fbcdc26de6ff62a53c75bd
Your Name <you@example.com>
2025-11-17 23:28:56 -0700

    Formatting

--- firewall.py
+++ firewall.py
@@ -409,7 +409,7 @@ def main():
                                 block_ip(ip_address, message)
                             else:
                                 # Prints any Web Traffic that does not fit into any of the filtering arrays above
-                                activity.append(f"🔎 {line.lower()}\n")
+                                activity.append(f"🔎 {line.lower()}")
 
         # Block IP's over the IP_OCCURRENCE_THRESHOLD
         # TIER_ONE Traffic does not count
