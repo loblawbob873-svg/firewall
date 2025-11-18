0e5647e673f03f6ff2f8cd15dd0f3905891cabf5
Your Name <you@example.com>
2025-11-18 13:14:39 -0700

    s

--- firewall.py
+++ firewall.py
@@ -509,6 +509,7 @@ def main():
                     URL = line.split("🔍");
                     URL_FIX = f"<a href=\"{URL[1]}\">🔍 IP Info></a>"
                     line = f"{URL[1]} {URL_FIX}"
+                    print(f"DEbug: {line}")
                 if "\t" in line:
                     line.replace("\t","") 
                 if "\t" in line:
