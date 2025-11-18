40e201ba35bc6939c08c001805fbefc63fbd0269
Your Name <you@example.com>
2025-11-17 23:28:23 -0700

    Formatting

--- firewall.py
+++ firewall.py
@@ -368,11 +368,11 @@ def main():
         )
 
         messaging(f"Searching logs for Time Stamp: {one_minute_ago}")
-        activity.append("\n---------------------------------------------")
+        activity.append("---------------------------------------------")
         activity.append("🔥 Python Firewall 🔥")
         activity.append(f"📋 Blocked IP's: {get_block_count()}")
+        activity.append(f"{get_cpu_usage()}")
         activity.append("----------------------------------------------")
-        activity.append(f"{get_cpu_usage()}\n")
         activity.append(f"⚠️ Unfiltered Traffic as of: {one_minute_ago}\n")
         with open(LOG_FILE, "r") as f:
             for line in f:
