6c325bef1d3091057b075a19d98c368555d465ff
Your Name <you@example.com>
2025-11-17 23:59:54 -0700

    s

--- firewall.py
+++ firewall.py
@@ -402,7 +402,7 @@ def main():
                                 word.lower() in line.lower() for word in SUBNET_BLOCKS
                             ):
                                 BIG_IP = extract_first_three_parts(ip_address)
-                                message = f"\t🚨 Blocked Subnet: 👉 {line.lower()}💀"
+                                message = f"\t🚨 Blocked Subnet: 👉 {line.lower()}"
                                 activity.append(message)
                                 block_ip(f"{BIG_IP}.0/24", message)
 
@@ -410,7 +410,7 @@ def main():
                             elif any(
                                 word.lower() in line.lower() for word in IP_BLOCKS
                             ):
-                                message = f"\t🚨 Blocked: 👉 {line.lower()}💀"
+                                message = f"\t🚨 Blocked: 👉 {line.lower()}"
                                 activity.append(message)
                                 block_ip(ip_address, message)
                             else:
