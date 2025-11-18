d9838b1be3c60300b9f96cb1510131be08a18435
Your Name <you@example.com>
2025-11-17 22:39:42 -0700

    s

--- firewall.py
+++ firewall.py
@@ -368,10 +368,10 @@ def main():
         )
 
         messaging(f"Searching logs for Time Stamp: {one_minute_ago}")
-        activity.append("\n----------------------------------")
+        activity.append("\n--------------------------------------------")
         activity.append("🔥 Python Firewall 🔥")
         activity.append(f"\n⚠️ Unfiltered Traffic as of: {one_minute_ago}")
-        activity.append("----------------------------------")
+        activity.append("----------------------------------------------")
         
         with open(LOG_FILE, "r") as f:
             for line in f:
