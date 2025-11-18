2dd24d063b49471cdf6127970107ba5e257fadf9
Your Name <you@example.com>
2025-11-17 23:14:47 -0700

    s

--- firewall.py
+++ firewall.py
@@ -395,7 +395,7 @@ def main():
                                 word.lower() in line.lower() for word in SUBNET_BLOCKS
                             ):
                                 BIG_IP = extract_first_three_parts(ip_address)
-                                message = f"⏰ Blocked Subnet: 👉 {line.lower()}🔪🩸💀"
+                                message = f"🚨 Blocked Subnet: 👉 {line.lower()}🔪🩸💀"
                                 activity.append(message)
                                 block_ip(f"{BIG_IP}.0/24", message)
 
@@ -403,7 +403,7 @@ def main():
                             elif any(
                                 word.lower() in line.lower() for word in IP_BLOCKS
                             ):
-                                message = f"⏰ Blocked: 👉 {line.lower()}🔪🩸💀"
+                                message = f"🚨 Blocked: 👉 {line.lower()}🔪🩸💀"
                                 activity.append(message)
                                 block_ip(ip_address, message)
                             else:
@@ -419,7 +419,7 @@ def main():
             if count > 1:
                 activity.append(f"📍 {ip}: {count}")
             if count > IP_OCCURRENCE_THRESHOLD:
-                message = f"⏰ Blocked: {ip} with a count of {count}"
+                message = f"🚨 Blocked: {ip} with a count of {count}"
                 activity.append(message)
                 block_ip(ip, message)
         
