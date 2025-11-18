b2da754378abd1b445fc39db5ab447fdf1fe3fc8
Your Name <you@example.com>
2025-11-17 20:41:43 -0700

    s

--- firewall.py
+++ firewall.py
@@ -384,7 +384,8 @@ def main():
                 # print(f"{ip_address}: {count}")
                 message = f"Blocked: {ip} with a count of {count}"
                 block_ip(ip, message)
-              
+                if args.print:
+                    print(f"{ip}: {count}")
 
         messaging(f"Firewall sleeping for: {TIME_FRAME}")
 
