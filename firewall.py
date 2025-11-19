eb78a13120ae4fa1f65ec88814daa2c9f68eb4ee
Your Name <you@example.com>
2025-11-19 09:14:24 -0700

    s

--- firewall.py
+++ firewall.py
@@ -563,10 +563,10 @@ def main():
                   #🚨 Blocked Subnet: 👉 {ip_address} {line.lower().split(" ")[6]}\n"
                 if not args.print and "🚨 Blocked IP:" in line:
                     value = line.split(" ")
-                    line = f"<p>🚨 Blocked IP: <a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{value[3]}\">{value[3]}</a>  {value[4]} {value[5]}</p>"
+                    line = f"<p>🚨 Blocked IP: <a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{value[3]}\">{value[3]}</a>  {value[4]} {value[5]}  <a href=\"https://www.ip-tracker.org/lookup.php?ip={value[3]}\" style=\"text-decoration:none\" target=\"_blank\"> &nbsp;🔍 <a style=\"text-decoration:none\" href=\"https://who.is/whois-ip/ip-address/{value[3]}\" target=\"_blank\">🌐</a></p>"
                 if not args.print and "🚨 Blocked Subnet:" in line:
                     value = line.split(" ")
-                    line = f"<p>🚨 Blocked Subnet: <a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{value[3]}\">{value[3]}</a>  {value[4]} {value[5]} <a href=\"https://www.ip-tracker.org/lookup.php?ip={value[3]}\" style=\"text-decoration:none\" target=\"_blank\"> &nbsp;🔍 <a style=\"text-decoration:none\" href=\"https://who.is/whois-ip/ip-address/{value[3]}\" target=\"_blank\">🌐</a></p>"
+                    line = f"<p>🚨 Blocked Subnet: <a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{value[3]}\">{value[3]}</a>  {value[4]} {value[5]}"
                 if not args.print and "📍" in line:
                     value = line.split(":")
                     URL_FIX = line.split(" ")
