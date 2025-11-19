cc56adb30a7d5473aa494ccb2c3dc2f4891eb6f5
Your Name <you@example.com>
2025-11-19 09:44:05 -0700

    sd

--- firewall.py
+++ firewall.py
@@ -589,12 +589,12 @@ def main():
             f.write("<p><b>🚨 Blocked Traffic</b><p><br>")
             for line in blocked_array:
                 if "🚨" in line:
-                    f.write(f"<br>{line}</br>")
+                    f.write(f"<br>{line.replace("🚨","👉")}</br>")
             
             f.write("<p><b>🕵️Queries</b><p><br>")
             for line in standard_queries:
                 if "🕵️" in line:
-                    f.write(f"<br>{line}</br>")
+                     f.write(f"<br>{line.replace("🕵️","👉")}</br>")
                     
             f.write("<p><b>IP Counter</b><p><br>")
             for line in ip_counters:
