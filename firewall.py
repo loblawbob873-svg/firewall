c49fda69f50cbfa3ca77dbf72b68d8af17b5d696
Your Name <you@example.com>
2025-11-18 21:32:53 -0700

    s

--- firewall.py
+++ firewall.py
@@ -510,7 +510,7 @@ def main():
                                 ):
 
                                     BIG_IP = extract_first_three_parts(ip_address)
-                                    message = f"\t🚨 Blocked Subnet: 👉 {ip_address} {line.lower().split("]")[1]}\n\t\t\t\t\t\t\t🔍 https://www.ip-tracker.org/lookup.php?ip={ip_address}\n"
+                                    message = f"\t🚨 Blocked Subnet: 👉 {ip_address} {line.lower().split("]")[1].split("\"-\"")[0]}\n\t\t\t\t\t\t\t🔍 https://www.ip-tracker.org/lookup.php?ip={ip_address}\n"
                                     activity.append(message)
                                     block_ip(f"{BIG_IP}.0/24", message)
 
@@ -518,7 +518,7 @@ def main():
                                 elif any(
                                     word.lower() in line.lower() for word in IP_BLOCKS
                                 ):
-                                    message = f"\t🚨 Blocked: {ip_address} 👉 {line.lower().split("]")[1].replace("\"-\" \"-\"","").split("\"-\""[0])}\n\t\t\t\t\t\t\t🔍 https://www.ip-tracker.org/lookup.php?ip={ip_address}\n"
+                                    message = f"\t🚨 Blocked: {ip_address} 👉 {line.lower().split("]")[1].replace("\"-\" \"-\"","").split("\"-\""[0].split("\"-\"")[0])}\n\t\t\t\t\t\t\t🔍 https://www.ip-tracker.org/lookup.php?ip={ip_address}\n"
                                     activity.append(message)
                                     block_ip(ip_address, message)
                                 else:
