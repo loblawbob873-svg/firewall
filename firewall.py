193e754298ae8bb568767980d1406815ede1450f
Your Name <you@example.com>
2025-11-18 22:38:20 -0700

    s

--- firewall.py
+++ firewall.py
@@ -559,7 +559,7 @@ def main():
                 f.write("\n")
                 if not args.print and "🔍" in line:
                     URL = line.split("🔍")
-                    URL_FIX = f'<a target="blank" href="{URL[1].strip()}">🔍</a>  <a href="https://who.is/whois-ip/ip-address/{URL[1].split("=")[1]}" target="_blank">🌐</a>'
+                    URL_FIX = f'<a target="blank" href="{URL[1]}">🔍</a>  <a href="https://who.is/whois-ip/ip-address/{URL[1].split("=")[1]}" target="_blank">🌐</a>'
                     line = f"{URL[0]} {URL_FIX}"
                 if "\t" in line:
                     line.replace("\t", "")
