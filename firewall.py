ad8a76fb24389d5e55bcab881ba2bfad7c26d911
Your Name <you@example.com>
2025-11-19 09:59:23 -0700

    w

--- firewall.py
+++ firewall.py
@@ -598,7 +598,7 @@ def main():
             f.write("<h1><b>🕵️ &nbsp; Queries</b></h1><br>")
             for line in standard_queries:
                 if "🕵️" in line:
-                     f.write(f"<br>{line.replace("🕵️","👉")}</br>")
+                     f.write(f"<br>{line.replace("🕵️","💩")}</br>")
                     
             f.write("<h1><b>IP Counter</b></h1><br>")
             for line in ip_counters:
