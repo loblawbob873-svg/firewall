b44777a195f9126dbb1a5cd8b243e947fd5838ea
Your Name <you@example.com>
2025-11-19 10:11:40 -0700

    s

--- firewall.py
+++ firewall.py
@@ -598,7 +598,7 @@ def main():
                     f.write(f"<br>{line.replace("🚨","🛑")}</br>")
             f.write("</div>")                                    
             
-            f.write("<div class=\"split left\">")
+            f.write("<div class=\"split right\">")
             f.write("<h1><b>🕵️ &nbsp; Queries</b></h1><br>")
             for line in standard_queries:
                 if "🕵️" in line:
