507a81bf14c0748320bee11440ba0e8c7fae3670
Your Name <you@example.com>
2025-11-19 07:59:09 -0700

    w

--- firewall.py
+++ firewall.py
@@ -564,6 +564,11 @@ def main():
                     value = line.split(":")
                     URL_FIX = line.split(" ")
                     line = f"📍 <a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{URL_FIX[1]}\">{URL_FIX[1]}</a>  {value[1]}"
+                if not args.print and "🚨" in line:
+                    URL = line.split("🚨")
+                    URL_PARSE = line.split(" ")
+                    URL_FIX = line.split(" ")
+                    line = f"<a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a> {URL_PARSE[2]} <a href=\"https://www.ip-tracker.org/lookup.php?ip={URL_PARSE[1]}\" style=\"text-decoration:none\" target=\"_blank\">  🔍 <a style=\"text-decoration:none\" href=\"https://who.is/whois-ip/ip-address/{URL_PARSE[1]}\" target=\"_blank\">🌐</a>"
                 if not args.print and "🕵️" in line:
                     URL = line.split("🕵️")
                     URL_PARSE = line.split(" ")
