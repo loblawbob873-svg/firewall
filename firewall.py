6be9ed19c3132f2f2d142300f2bc8d06c346510a
Your Name <you@example.com>
2025-11-19 08:07:03 -0700

    s

--- firewall.py
+++ firewall.py
@@ -560,15 +560,14 @@ def main():
                 GET_AI_IP = ""
                 print(f"\n{line}")
                 f.write("\n")
+                if not args.print and "🚨" in line:
+                    value = line.split(":")
+                    URL_FIX = line.split(" ")
+                    line = f"🚨 BLocked: <a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{URL_FIX[1]}\">{URL_FIX[1]}</a>  {value[1]}"
                 if not args.print and "📍" in line:
                     value = line.split(":")
                     URL_FIX = line.split(" ")
                     line = f"📍 <a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{URL_FIX[1]}\">{URL_FIX[1]}</a>  {value[1]}"
-                if not args.print and "🚨" in line:
-                    URL = line.split("🚨")
-                    URL_PARSE = line.split(" ")
-                    URL_FIX = line.split(" ")
-                    line = f"<a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{URL_FIX[2]}\">🚨 {URL_FIX[1]}</a> {URL_PARSE[3]} <a href=\"https://www.ip-tracker.org/lookup.php?ip={URL_PARSE[1]}\" style=\"text-decoration:none\" target=\"_blank\">  🔍 <a style=\"text-decoration:none\" href=\"https://who.is/whois-ip/ip-address/{URL_PARSE[1]}\" target=\"_blank\">🌐</a>"
                 if not args.print and "🕵️" in line:
                     URL = line.split("🕵️")
                     URL_PARSE = line.split(" ")
