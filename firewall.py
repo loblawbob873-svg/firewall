864941c833bfad08c325cfaae42b2416214600e1
Your Name <you@example.com>
2025-11-19 13:27:32 -0700

    ss

--- firewall.py
+++ firewall.py
@@ -591,7 +591,7 @@ def main():
             f.write(
                 "<script>\nwindow.setTimeout( function() {window.location.reload();}, 32000);</script>"
             )
-            f.write("<body><header>🔥 Python Firewall Web Console 🔥<br> <br>")          
+            f.write("<body><header><h2>🔥 Python Firewall Web Console 🔥</h2><br> <br>")          
             f.write(f"<p align=center>{get_cpu_usage()}\tBlocked IP's: {get_block_count().strip()} ✅</header")
             f.write(
             f"<br><p align=center> ↕️Traffic as of: {one_minute_ago}</p></div>"
