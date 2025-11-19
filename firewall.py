41cf47ad56d2f18faf241de3418daf93f6504f53
Your Name <you@example.com>
2025-11-18 20:25:50 -0700

    s

--- firewall.py
+++ firewall.py
@@ -546,7 +546,7 @@ def main():
         os.system("clear")
 
         with open(WEB_HTML, "w") as f:
-            f.write("<html><head><style> body { font-family: Arial, sans-serif; text-align: center; } #stats { font-size: 1em; margin-top: 50px; }</style></head>")
+            f.write("<html><head><style> body { font-family: Arial, sans-serif; text-align: left; } #stats { font-size: 1em; margin-top: 50px; }</style></head>")
             f.write(
                 "<script>\nwindow.setTimeout( function() {window.location.reload();}, 32000);</script>"
             )
