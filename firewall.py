005b4bac17ae431d30421b6830f1175549860ff7
Your Name <you@example.com>
2025-11-19 09:00:57 -0700

    s

--- firewall.py
+++ firewall.py
@@ -575,7 +575,7 @@ def main():
                     URL = line.split("🕵️")
                     URL_PARSE = line.split(" ")
                     URL_FIX = line.split(" ")
-                    line = f"<a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a> {URL_PARSE[2]} <a href=\"https://www.ip-tracker.org/lookup.php?ip={URL_PARSE[1]}\" style=\"text-decoration:none\" target=\"_blank\">  🔍 <a style=\"text-decoration:none\" href=\"https://who.is/whois-ip/ip-address/{URL_PARSE[1]}\" target=\"_blank\">🌐</a>"
+                    line = f"<a style=\"text-decoration:none\" target=\"_blank\" href=\"https://{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a>👉 {URL_PARSE[2]} <a href=\"https://www.ip-tracker.org/lookup.php?ip={URL_PARSE[1]}\" style=\"text-decoration:none\" target=\"_blank\">  🔍 <a style=\"text-decoration:none\" href=\"https://who.is/whois-ip/ip-address/{URL_PARSE[1]}\" target=\"_blank\">🌐</a>"
                 if "\t" in line:
                     line.replace("\t", "")
                 if "\n" in line:
