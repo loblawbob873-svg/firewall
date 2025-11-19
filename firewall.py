45c120b84d284fc8b3ecc62ebc36503aca1717b4
Your Name <you@example.com>
2025-11-18 23:38:10 -0700

    s

--- firewall.py
+++ firewall.py
@@ -569,7 +569,7 @@ def main():
                     URL = line.split("🕵️")
                     URL_PARSE = line.split(" ")
                     URL_FIX = line.split(" ")
-                    line = f"<a target=\"blank\" href=\"https://{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a> {URL_PARSE[3]} <a href="https://who.is/whois-ip/ip-address/{URL[1].split("=")[1]}" target="_blank">🌐</a>"
+                    line = f"<a target=\"blank\" href=\"https://{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a> {URL_PARSE[3]} <a href=\"https://who.is/whois-ip/ip-address/{URL[1].split("=")[1]}\" target=\"_blank\">🌐</a>"
                 if "\t" in line:
                     line.replace("\t", "")
                 if "\n" in line:
