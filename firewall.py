422b50253423e3578f36ba8034195312feb2ab35
Your Name <you@example.com>
2025-11-18 23:25:02 -0700

    s

--- firewall.py
+++ firewall.py
@@ -566,7 +566,7 @@ def main():
                     URL = line.split("🕵️")
                     URL_PARSE = line.split(" ")
                     URL_FIX = line.split(" ")
-                    line = f"<a target=\"blank\" href=\"https://{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a> {URL_PARSE[2]}"
+                    line = f"<a target=\"blank\" href=\"https://{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a> {line}"
                 if not args.print and "🔍" in line:
                     URL = line.split("🔍")
                     URL_FIX = f'<a target="blank" href="{URL[1]}">🔍</a>  <a href="https://who.is/whois-ip/ip-address/{URL[1].split("=")[1]}" target="_blank">🌐</a>'
