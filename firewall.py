ecf5e9a370be61d8b44cbbe8c35f943fec026c83
Your Name <you@example.com>
2025-11-19 13:36:30 -0700

    s

--- firewall.py
+++ firewall.py
@@ -585,7 +585,7 @@ def main():
                     line.replace("\n", "<br>")                 
                     
         with open(WEB_HTML, "w") as f:
-            f.write("<html><head><style> p { text-indent: 50px; } body {  background-color: black; font-family: Arial, sans-serif; text-align: left; } #stats { font-size: 2em; margin-top: 50px; }")
+            f.write("<html><head><style> p { text-indent: 50px; } #stats { font-size: 2em; margin-top: 50px; }")
             f.write("body {  background-color: black; font-family: Arial, sans-serif; text-align: left; display: flex; flex-direction: column; height: 100vh; margin: 0; } header { background-color: #f8f9fa; padding: 20px; text-align: center; } main { display: flex; flex: 1; } aside, article, nav { flex: 1; border: 1px solid #ddd; box-sizing: border-box; }")
             f.write("</style></head>")
             f.write(
