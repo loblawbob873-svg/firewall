a2eebc5a6b846c1ae069b38473dff1f0b819751e
Your Name <you@example.com>
2025-11-18 22:56:15 -0700

    s

--- firewall.py
+++ firewall.py
@@ -556,6 +556,9 @@ def main():
                 GET_AI_IP = ""
                 print(f"\n{line}")
                 f.write("\n")
+                if not args.print and "🕵️" in line:
+                    URL_FIX = line.split(" ")
+                    line = f"<a target=\"blank\" href=\"{URL_FIX[1]}\">🕵️ {URL_FIX[1]}</a>"
                 if not args.print and "🔍" in line:
                     URL = line.split("🔍")
                     URL_FIX = f'<a target="blank" href="{URL[1]}">🔍</a>  <a href="https://who.is/whois-ip/ip-address/{URL[1].split("=")[1]}" target="_blank">🌐</a>'
