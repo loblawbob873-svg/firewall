43caa7a50635121cfb423837c89acae0308f43af
Your Name <you@example.com>
2025-11-19 12:49:08 -0700

    s

--- firewall.py
+++ firewall.py
@@ -514,7 +514,7 @@ def main():
                                     BIG_IP = extract_first_three_parts(ip_address)
                                     gather_data = line.lower().split(" ")[6]
                                     clean_data = textwrap.shorten(gather_data, width=20, placeholder="...")
-                                    message = f"\t🚨 Blocked Subnet: {ip_address} 👉 {clean_data}\n"
+                                    message = f"\t🚨 Blocked Subnet: {ip_address} 👉 { wrapper.fill(text=clean_data)}\n"
                                     activity.append(message)
                                     block_ip(f"{BIG_IP}.0/24", message)
 
@@ -525,7 +525,7 @@ def main():
                                     gather_data = line.lower().split(" ")[6]
                                     clean_data = textwrap.shorten(gather_data, width=20, placeholder="...")
                                     print(clean_data)
-                                    message = f"\t🚨 Blocked IP: {ip_address} 👉 {clean_data}\n"
+                                    message = f"\t🚨 Blocked IP: {ip_address} 👉 { wrapper.fill(text=clean_data)}\n"
                                     activity.append(message)
                                     block_ip(ip_address, message)
                                 else:
