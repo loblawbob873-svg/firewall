4137032db231fd5c55bb3ec6a553a5878ec31c8b
Your Name <you@example.com>
2025-11-20 10:09:33 -0700

    s

--- firewall.py
+++ firewall.py
@@ -387,7 +387,7 @@ async def main(ip: str, date: str):
         content += "function loopThroughArray(arr) {"
         content += "let output = document.getElementById('output');"
         content += "for (let i = 0; i < arr.length; i++) {"
-        content += "output.innerHTML += `$\{arr[i]\}\n`;"
+        content += "output.innerHTML += `${arr[i]}\n`;"
         content += '} }'
         content += f"const myArray = {array};"
         content += "loopThroughArray(myArray);</script>"
