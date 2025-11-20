dddc3ff6ebe0df6649ae676d1952b97b88afbc45
Your Name <you@example.com>
2025-11-20 13:40:54 -0700

    s

--- firewall.py
+++ firewall.py
@@ -534,26 +534,25 @@ def buildWeb(activity,one_minute_ago):
         if "\n" in line:
             line.replace("\n", "<br>")
 
-        with open(WEB_HTML, "w") as f:
-            f.write(basicHTML(one_minute_ago))
-            f.write(htmlRELOAD())
-            f.write("<main><aside><h2><b>🚨 &nbsp; Blocked Traffic</b></h1><br></h2>")
-            for line in blocked_array:
-                if "🚨" in line:
-                    f.write(f"<br>{line.replace("🚨","🛑")}</br>")
-            f.write("</aside>")
-
-            f.write("<article><h2><b>🕵️ &nbsp; Queries</b></h2><br>")
-            for line in standard_queries:
-                if "🕵️" in line:
-                    f.write(f"<br>{line.replace("🕵️","⁉️")}</br>")
-            f.write("</article>")
-
-            f.write("<nav><h2><b>🧮 IP Counter</b></h2><br>")
-            for line in ip_counters:
-                if "📍" in line:
-                    f.write(f"<br>{line}</br>")
-            f.write("</nav></main></body></html>")
+    with open(WEB_HTML, "w") as f:
+        f.write(basicHTML(one_minute_ago))
+        f.write(htmlRELOAD())
+        f.write("<main><aside><h2><b>🚨 &nbsp; Blocked Traffic</b></h1><br></h2>")
+        for line in blocked_array:
+            if "🚨" in line:
+                f.write(f"<br>{line.replace("🚨","🛑")}</br>")
+                f.write("</aside>")
+        f.write("<article><h2><b>🕵️ &nbsp; Queries</b></h2><br>")
+        for line in standard_queries:
+            if "🕵️" in line:
+                f.write(f"<br>{line.replace("🕵️","⁉️")}</br>")
+                f.write("</article>")
+        
+        f.write("<nav><h2><b>🧮 IP Counter</b></h2><br>")
+        for line in ip_counters:
+            if "📍" in line:
+                f.write(f"<br>{line}</br>")
+                f.write("</nav></main></body></html>")
 
 def main():
     parser = argparse.ArgumentParser(description="Firewall Script")
@@ -571,7 +570,7 @@ def main():
 
         if not args.print:
             buildWeb(activity,one_minute_ago)
-            
+
         if args.print:
             activity.append(
                 "-------------------------------------------------------------------------------------------------"
