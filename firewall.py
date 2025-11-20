2c18539e9ba1d8bea9252cd00c0bea579477a085
Your Name <you@example.com>
2025-11-20 09:49:10 -0700

    s

--- firewall.py
+++ firewall.py
@@ -386,7 +386,7 @@ async def main(ip: str, date: str):
         content += "let output = document.getElementById('output');"
         content += "for (let i = 0; i < arr.length; i++) {"
         #content += "output.innerHTML += `${arr[i]\}n`;"
-        content += "}}"
+        #content += "}}"
         content += f"const myArray = {array};"
         content += "loopThroughArray(myArray);"
         return HTMLResponse(content=conent)
