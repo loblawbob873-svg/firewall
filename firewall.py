9ea0a06faba4366818c164f0ae4bbbf7628ad5dd
Your Name <you@example.com>
2025-11-20 12:47:54 -0700

    s

--- firewall.py
+++ firewall.py
@@ -518,20 +518,20 @@ def buildWeb(activity):
     ip_counters = []
 
     for line in activity:
-        if not args.print and "🚨 Blocked IP:" in line:
+        if "🚨 Blocked IP:" in line:
             value = line.split(" ")
             line = f'<p>🚨<a style="text-decoration:none" target="_blank" href="https://{value[3]}">{value[3]}</a>  {value[4]} {value[5]} <a target="_blank" href="/ip?ip={value[3]}&date={one_minute_ago}" style="text-decoration:none"> 🔍</a></p>'
             blocked_array.append(line)
-        if not args.print and "🚨 Blocked Subnet:" in line:
+        if "🚨 Blocked Subnet:" in line:
             value = line.split(" ")
             line = f'<p>🚨<a style="text-decoration:none" target="_blank" href="https://{value[3]}">{value[3]}</a>  {value[4]} {value[5]} <a target="_blank" href="/ip?ip={value[3]}&date={one_minute_ago}" style="text-decoration:none">🔍 </a> </a> </p>'
             blocked_array.append(line)
-        if not args.print and "📍" in line:
+        if "📍" in line:
             value = line.split(" ")
             URL_FIX = line.split(" ")
             line = f'<p>📍 <a style="text-decoration:none" target="_blank" href="https://{URL_FIX[1]}">{URL_FIX[1]}</a>&nbsp;{value[2]} <a target="_blank" href="/ip?ip={URL_FIX[1]}&date={one_minute_ago}" style="text-decoration:none"> 🔍</a></p>'
             ip_counters.append(line)
-        if not args.print and "🕵️" in line:
+        if "🕵️" in line:
             URL = line.split("🕵️")
             URL_PARSE = line.split(" ")
             URL_FIX = line.split(" ")
