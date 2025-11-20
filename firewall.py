0d20b732304a9d145cd91a855b8a998a0760e2a7
Your Name <you@example.com>
2025-11-20 09:18:32 -0700

    s

--- firewall.py
+++ firewall.py
@@ -370,7 +370,7 @@ async def main(ip: str, date: str):
         with open(f"{LOG_FILE}", "r") as f:
             for line in f:
                 if date and ip in line:
-                    array.append(line.lower().split(" ")[6])
+                    array.append(line.lower())
         # return HTMLResponse(content=array)
         return array
     except Exception as e:
