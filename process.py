acbd1f1fb38395dde67497c92a448cab886f45c5
Your Name <you@example.com>
2025-11-21 12:18:59 -0700

    a

--- process.py
+++ process.py
@@ -11,6 +11,7 @@ from tier_one import TIER_ONE
 from ip_blocks import IP_BLOCKS
 from config import SUBNET_BLOCKS
 from config import LOCAL_NETWORK
+from db import ip_counts
 
 def extract_first_three_parts(ip):
     return ".".join(ip.split(".")[:3])
