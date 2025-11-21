2bec5c790b8f5a181472420f91bfce3003ac82d1
Your Name <you@example.com>
2025-11-20 19:54:24 -0700

    s

--- db.py
+++ db.py
@@ -1,12 +1,10 @@
 activity = []
 ip_counts = {}  # Dictionary to store IP addresses and their occurrence counts
 
-def addActivity(data){
+def addActivity(data):
     global activity
     activity.append(data)
-}
 
-def getActivity(data){
+def getActivity(data):
     global activity
     return data
-}
