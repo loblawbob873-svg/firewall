02c6850dc914aa017049c356d2a3f9f86357c1fa
Your Name <you@example.com>
2025-11-20 21:03:03 -0700

    s

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ SKIP_ALERTS = [
 ]
 
 # NTFY_URL=""
-NTFY_URL = ""
-# NTFY_URL = "https://push.poster.place/logs"
+#NTFY_URL = ""
+NTFY_URL = "https://push.poster.place/logs"
 
 # Web Inferface HTML File
 WEB_HTML = "/tmp/python-firewall.html"
