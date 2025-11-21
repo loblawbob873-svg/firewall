4642eb9b271ca9f3906b9a4877ef5382cb2fcc13
Your Name <you@example.com>
2025-11-21 12:11:56 -0700

    s

--- process.py
+++ process.py
@@ -5,9 +5,7 @@ from config import LOG_FILE
 from config import IP_OCCURRENCE_THRESHOLD
 from db import activity
 from db import addActivity
-from db import getIPCOUNTS
 from db import clearIPCOUNTS
-from db import addIPCOUNTS
 from commands import block_ip
 from tier_one import TIER_ONE
 from ip_blocks import IP_BLOCKS
