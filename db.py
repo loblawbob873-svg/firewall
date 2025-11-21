b58f26354ed693229febe36ddec6c75c74af1089
Your Name <you@example.com>
2025-11-21 12:08:09 -0700

    s

--- db.py
+++ db.py
@@ -3,18 +3,10 @@ html = []
 ip_counts = {}  # Dictionary to store IP addresses and their occurrence counts
 
 
-def addIPCOUNTS(data):
-    global ip_counts
-    ip_counts.append(data)
-
 def clearIPCOUNTS():
     global ip_counts
     ip_counts = []
     
-def getIPCOUNTS(data):
-    global ip_counts
-    return ip_counts
-
 def addActivity(data):
     global activity
     activity.append(data)
