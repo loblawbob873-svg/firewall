946487b9b3802fc0ae95ed22cd4314159c2af1dd
Your Name <you@example.com>
2025-11-21 11:33:56 -0700

    d

--- api.py
+++ api.py
@@ -28,7 +28,7 @@ async def main():
     for line in getHTML():
         DATA+= line
     if not DATA:
-        content = f(" {basicHTML} Web Interface not ready yet.")
+        content = (f" {basicHTML} Web Interface not ready yet.")
         return HTMLResponse(content=content)
     else:
         return HTMLResponse(content=DATA)
